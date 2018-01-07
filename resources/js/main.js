20c2e57d7503cd78a6150fd4c895e3326af10734
alohawahine059 <alohawahine059@gmail.com>
2018-01-07 15:02:12 -0700

    Jumpstart HTML/CSS submission.

---
+++ resources/js/main.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+	// carousel options
+	$('.carousel').slick({
+		slidesToShow: 1,
+		autoplay: false,
+		autoplaySpeed: 3000,
+		arrows: false,
+		draggable: false,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+	});
+
+	// Hide nav bar when scroll (x)
+	var pageScroll = 0;
+	// when the window scrolls run function
+	$( window ).scroll(function() {
+		// Get current vertical position of scroll bar of this(window) (y)
+		var scrollTop = $(this).scrollTop(); 
+		// if statement is true, create variable
+		if (scrollTop > (pageScroll + 50)) {
+			var navHeight = $('.nav').height();
+			// console.log(navHeight);
+			$('.nav').animate({
+				top: -navHeight}, 150);
+			pageScroll = scrollTop;
+		} else if (pageScroll > (scrollTop + 50)) {
+				$('.nav').animate({
+				top: 0}, 150);
+				pageScroll = scrollTop;
+		}
+	});
+});
